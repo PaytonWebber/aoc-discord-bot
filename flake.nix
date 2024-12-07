6108834b727072863492a1e9ee6dacf0fbbe3f33
Nick <nicklausb@shaw.ca>
2024-12-06 19:39:44 -0800

    flake added

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "go flake";
+
+  inputs = {
+      nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... } @ inputs:
+	let
+	system = "x86_64-linux";
+	pkgs = import inputs.nixpkgs {inherit system;};
+	in {
+	    devShells.${system}.go = pkgs.mkShell {
+		buildInputs = with pkgs; [
+			go 
+			gcc
+			libcap
+		];
+
+	    shellHook = ''
+	    	echo "Entered Go Dev environment"
+		'';
+	};
+  };
+}
